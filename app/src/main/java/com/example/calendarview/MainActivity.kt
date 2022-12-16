fdec25121a24830518f8e744230cd0ba28f8147d
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-12-16 14:24:51 +0100

    Zbudowanie layoutu, dodanie ustawienie minimalnej i maksymalnej daty

--- app/src/main/java/com/example/calendarview/MainActivity.kt
+++ app/src/main/java/com/example/calendarview/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.calendarview
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.ZoneId
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var clndView = findViewById<CalendarView>(R.id.calendarView)
+        var wyjbtn = findViewById<Button>(R.id.wyjbtn)
+        var przybtn = findViewById<Button>(R.id.przybtn)
+        var wyjlbl = findViewById<TextView>(R.id.wyjlbl)
+        var przylbl = findViewById<TextView>(R.id.przylbl)
+        var ilosclbl = findViewById<TextView>(R.id.ilosclbl)
+
+        clndView.minDate = System.currentTimeMillis()
+        clndView.maxDate = LocalDate.now().plusYears(2).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+
+
+
+
+
     }
 }
